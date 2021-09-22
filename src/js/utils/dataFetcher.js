f5b0aa792f3c29fd4a3f2981368dc706ea242197
Zachary Fedorak <zfedorak1@nait.ca>
2021-09-22 11:49:08 -0600

    added all examples from last class

---
+++ src/js/utils/dataFetcher.js
@@ -0,0 +1,30 @@
+const dataFetcher = async function (url=null) {
+//this understood 
+//check the url value !null
+// throw an error
+// get request to the url get something http verbs
+
+const res = await fetch(url)
+const data = await res.json()
+//.then(res=> res.json())
+//.then(data=> data)
+
+return data
+}
+
+export {dataFetcher}
+/*
+const dataFetchers = () => {
+explict return
+this is limited to none
+return 1
+}
+
+function zack(){
+named function
+}
+
+immidiatly returned
+const temp ()=> <div>12</div>
+
+*/
